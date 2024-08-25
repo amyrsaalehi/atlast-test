df62d862635f6ef4d72fe007196c76e659253a6f
Amir Salehi <amirsalehi@Amirs-MacBook-Pro.local>
2024-08-25 00:12:49 -0700

    feat:option click handled and handle errors

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,19 +8,25 @@ const CustomInput = () => {
   const [inputValue, setInputValue] = useState("");
   const [selectedOption, setSelectedOption] = useState(defaultOptions[0]);
   const [showOptions, setShowOptions] = useState(defaultOptions);
+  const [showError, setShowError] = useState(false);
 
-  const handleFocus = () => {
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.target.select();
     setShowDropdown(true);
   };
 
   const handleBlur = () => {
-    setShowDropdown(false);
+    const optionFound = defaultOptions.find(
+      (option) => option.value === inputValue,
+    );
+    if (!optionFound) {
+      setShowError(true);
+    } else {
+      setShowError(false);
+    }
   };
 
-  const handleInputTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const text = e.target.value.trim();
-    setInputValue(text);
-
+  const handleFilterOptions = (text: string) => {
     if (text === "") {
       setShowOptions(defaultOptions);
     } else {
@@ -31,10 +37,17 @@ const CustomInput = () => {
     }
   };
 
+  const handleInputTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const text = e.target.value.trim();
+    setInputValue(text);
+    handleFilterOptions(text);
+  };
+
   const handleOptionClick = (option: (typeof defaultOptions)[0]) => () => {
     setInputValue(option.value);
     setSelectedOption(option);
     setShowDropdown(false);
+    handleFilterOptions(option.value);
   };
 
   return (
@@ -51,7 +64,9 @@ const CustomInput = () => {
           id="textInput"
           value={inputValue}
           onChange={handleInputTextChange}
-          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:shadow-lg transition-all"
+          className={`w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:shadow-lg transition-all ${
+            showError ? "ring ring-red-500" : ""
+          }`}
           placeholder="Enter your text here"
           onFocus={handleFocus}
           onBlur={handleBlur}
@@ -76,10 +91,6 @@ const CustomInput = () => {
             )}
           </div>
         )}
-        <p>
-          Selected Option:{" "}
-          <span className="font-bold">{selectedOption.value}</span>
-        </p>
       </div>
     </div>
   );
