5ad4680219abf19a64608de35b04b8e06e0b076a
Amir Salehi <amirsalehi@Amirs-MacBook-Pro.local>
2024-08-24 23:55:38 -0700

    feat:text input filter the dropdown

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,13 @@
 "use client";
 
+import { defaultOptions } from "@/mock/data";
 import { useState } from "react";
 
 const CustomInput = () => {
-  const [inputValue, setInputValue] = useState("");
-  const defaultOptions = [
-    "Education 🎓",
-    "Science ⚗️",
-    "Art 🎭",
-    "Sport ⚽️",
-    "Games 🎮",
-  ];
-  const [selectedOption, setSelectedOption] = useState(defaultOptions[0]);
   const [showDropdown, setShowDropdown] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [selectedOption, setSelectedOption] = useState(defaultOptions[0]);
+  const [showOptions, setShowOptions] = useState(defaultOptions);
 
   const handleFocus = () => {
     setShowDropdown(true);
@@ -22,8 +17,28 @@ const CustomInput = () => {
     setShowDropdown(false);
   };
 
+  const handleInputTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const text = e.target.value.trim();
+    setInputValue(text);
+
+    if (text === "") {
+      setShowOptions(defaultOptions);
+    } else {
+      const filteredOptions = defaultOptions.filter((option) =>
+        option.label.toLowerCase().includes(text.toLowerCase()),
+      );
+      setShowOptions(filteredOptions);
+    }
+  };
+
+  const handleOptionClick = (option: (typeof defaultOptions)[0]) => () => {
+    setInputValue(option.value);
+    setSelectedOption(option);
+    setShowDropdown(false);
+  };
+
   return (
-    <div className="w-full max-w-md">
+    <div className="w-full max-w-xs">
       <div className="relative">
         <label
           htmlFor="textInput"
@@ -35,7 +50,7 @@ const CustomInput = () => {
           type="text"
           id="textInput"
           value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+          onChange={handleInputTextChange}
           className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:shadow-lg transition-all"
           placeholder="Enter your text here"
           onFocus={handleFocus}
@@ -43,23 +58,28 @@ const CustomInput = () => {
           autoComplete="off"
         />
         {showDropdown && (
-          <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg">
-            {defaultOptions.map((option) => (
-              <div
-                key={option}
-                className="px-3 py-2 cursor-pointer hover:bg-gray-100"
-                onClick={() => {
-                  console.log(option);
-                  setSelectedOption(option);
-                  setInputValue(option);
-                  setShowDropdown(false);
-                }}
-              >
-                {option}
+          <div className="absolute w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg">
+            {showOptions.length ? (
+              showOptions.map((option) => (
+                <div
+                  key={option.id}
+                  className="px-6 py-3 cursor-pointer hover:bg-gray-100 text-sm"
+                  onClick={handleOptionClick(option)}
+                >
+                  {option.label}
+                </div>
+              ))
+            ) : (
+              <div className="px-6 py-3 text-sm text-center">
+                No option found
               </div>
-            ))}
+            )}
           </div>
         )}
+        <p>
+          Selected Option:{" "}
+          <span className="font-bold">{selectedOption.value}</span>
+        </p>
       </div>
     </div>
   );
---
+++ src/mock/data.ts
@@ -0,0 +1,27 @@
+export const defaultOptions = [
+  {
+    id: 1,
+    label: "Education 🎓",
+    value: "Education 🎓",
+  },
+  {
+    id: 2,
+    label: "Science ⚗️",
+    value: "Science ⚗️",
+  },
+  {
+    id: 3,
+    label: "Art 🎭",
+    value: "Art 🎭",
+  },
+  {
+    id: 4,
+    label: "Sport ⚽️",
+    value: "Sport ⚽️",
+  },
+  {
+    id: 5,
+    label: "Games 🎮",
+    value: "Games 🎮",
+  },
+];
